41a2e25cd51e27254ea1b6883af58002f4d08ad6
niesselu <lukas.niessen@ista.com>
2025-06-02 20:39:16 +0200

    Updated to latest version

--- src/tests/layered-arch.spec.ts
+++ src/tests/layered-arch.spec.ts
@@ -2,27 +2,21 @@ import { projectFiles } from 'archunit';
 import { describe, it, expect } from 'vitest';
 
 describe('architecture', () => {
-	// architecture tests can take a while to finish
-	// Vitest default timeout is 5000ms. If needed, configure in vitest.config.ts or use test.setTimeout
-
-	// we use async await in combination with jest since this project uses asynchronous calls
-	it('business logic should not depend on the ui', async () => {
+	it.only('business logic should not depend on the ui', async () => {
 		const rule = projectFiles()
-			.inFolder('business')
-			.shouldNot() // violation on purpose, should be "shouldNot"
+			.inFolder('src/business')
+			.shouldNot()
 			.dependOnFiles()
-			.inFolder('ui');
+			.inFolder('src/services');
 
 		await expect(rule).toPassAsync();
 	});
 
 	it('business logic should be cycle free', async () => {
 		const rule = projectFiles().inFolder('business').should().haveNoCycles();
-
 		await expect(rule).toPassAsync();
 	});
 
-	// we use async await in combination with jest since this project uses asynchronous calls
 	it('ui logic should not depend on database logic', async () => {
 		const rule = projectFiles()
 			.inFolder('ui')
@@ -33,7 +27,6 @@ describe('architecture', () => {
 		await expect(rule).toPassAsync();
 	});
 
-	// we use async await in combination with jest since this project uses asynchronous calls
 	it('ui logic should not depend on database logic', async () => {
 		const rule = projectFiles()
 			.inFolder('ui')
